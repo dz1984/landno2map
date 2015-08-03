b262e579cb3e35bffeba71f8d5e8b950c79501f9
Donald Zhan <donald.zhan1984@gmail.com>
2015-08-03 15:23:38 +0800

    add new api

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -7,3 +7,11 @@
 
 <h3> TODO: show the summary list.</h3>
 @stop
+
+@section('script')
+<script>
+    $.getJson('api/land/list',function(data) {
+        console.log(data);
+    });
+</script>
+@stop
