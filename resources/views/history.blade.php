b7b0d755b1f2d1ed5003d4fabef8c84ba902f878
Donald Zhan <donald.zhan1984@gmail.com>
2015-08-16 08:48:55 +0800

    move the script block

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -6,11 +6,16 @@
 <h1>History</h1>
 
 <h3> TODO: show the summary list.</h3>
+<div id="render-list"></div>
 @stop
 
 @section('script')
+<script type="text/template" id="content-tpl">
+</script>
 <script>
-    $.getJson('api/land/list',function(data) {
+    $.getJSON('api/land/list',function(data) {
+        // TODO : render the lands records
+        //
         console.log(data);
     });
 </script>
--- resources/views/includes/script.blade.php
+++ resources/views/includes/script.blade.php
@@ -4,39 +4,3 @@
 <script src="js/libs/bootstrap.min.js"></script>
 <script src="js/libs/ripples.min.js"></script>
 <script src="js/libs/material.min.js"></script>
-
-<script>
-  $.material.init();
-</script>
-
-<script type="text/template" id="item-tpl">
-    <tr><td><%=land.k%></td><td><%=land.v%></td></tr>
-</script>
-
-<script type="text/template" id="content-tpl">
-
-<div class="modal fade">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
-                    X
-                </button>
-            </div>
-            <div class="modal-body">
-                <div class="row">
-                    <div class="col-md-12">
-                         <table class="table table-striped">
-                         <%=_.reduce(land_info,function(content, land){
-                           var content_compiled = _.template($('#item-tpl').html());
-                           var land_content = content_compiled({land:land});
-                           return content+land_content;
-                         },"")%>
-                         </table> 
-                    </div><!-- end col -->
-                </div><!-- end row -->
-            </div><!-- end modal-body -->
-        </div><!-- end modal-content -->
-    </div><!-- end modal-dialog -->
-</div><!-- end modal -->
-</script>
